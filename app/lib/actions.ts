f6ed50caf1271d08f256f9180c8892e3b7519784
Mr Blackest <yroslavfomin.2007@gmail.com>
2024-04-26 10:55:16 +0300

    6.5

---
+++ app/lib/actions.ts
@@ -0,0 +1,36 @@
+'use server';
+
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+// Define the form schema using Zod for validation
+const FormSchema = z.object({
+  id: z.string(),
+  sellerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['awaiting', 'fulfilled']),
+  date: z.string(),
+});
+
+// Define specific schemas for creating invoices
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+
+export async function createInvoice(formData: FormData) {
+    const { sellerId, amount, status } = CreateInvoice.parse({
+      sellerId: formData.get('sellerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+    await sql`
+    INSERT INTO invoices (seller_id, amount, status, date)
+    VALUES (${sellerId}, ${amountInCents}, ${status}, ${date})
+    `;
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+  }
+
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -10,6 +10,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
   function handleSearch(term: string) {
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
 
     if (term) {
       params.set('query', term);
