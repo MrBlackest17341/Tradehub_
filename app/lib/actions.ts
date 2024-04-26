66fcc78e008a8be552db732d33cf9d0ae8180f1f
Mr Blackest <yroslavfomin.2007@gmail.com>
2024-04-26 11:49:49 +0300

    7.2

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -16,16 +16,17 @@ const FormSchema = z.object({
 
 // Define specific schemas for creating invoices
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 
 export async function createInvoice(formData: FormData) {
-    const { sellerId, amount, status } = CreateInvoice.parse({
-      sellerId: formData.get('sellerId'),
-      amount: formData.get('amount'),
-      status: formData.get('status'),
+  const { sellerId, amount, status } = CreateInvoice.parse({
+    sellerId: formData.get('sellerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
     });
-    const amountInCents = amount * 100;
-    const date = new Date().toISOString().split('T')[0];
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
     await sql`
     INSERT INTO invoices (seller_id, amount, status, date)
     VALUES (${sellerId}, ${amountInCents}, ${status}, ${date})
@@ -33,4 +34,29 @@ export async function createInvoice(formData: FormData) {
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
   }
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { sellerId, amount, status } = UpdateInvoice.parse({
+    sellerId: formData.get('sellerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET seller_id = ${sellerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
+}
+  
 
