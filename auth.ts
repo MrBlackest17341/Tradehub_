bc6497bf5ec23142774bd017cfb001797b9e2a1e
Mr Blackest <yroslavfomin.2007@gmail.com>
2024-04-26 19:38:52 +0300

    7.3-7.5

---
+++ auth.ts
@@ -0,0 +1,9 @@
+import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
+import Credentials from 'next-auth/providers/credentials';
+
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  providers: [Credentials({})],
+});
+  
